471a8497d1aeeae0e9ae1a32faba55d873be152b
lNalator <enzomidonetpro@gmail.com>
2022-05-02 16:36:48 +0200

    EM : added teacher generation backend

--- my-app/my-back/getUsers.js
+++ my-app/my-back/getUsers.js
@@ -8,8 +8,8 @@ var connection = mysql.createConnection({
   database: "pronote_back",
 });
 
-var studentsUrl = "https://randomuser.me/api/?results=1000&nat=fr";
-var teacherUrl = "https://randomuser.me/api/?results=5&nat=fr";
+var studentsUrl = "https://randomuser.me/api/?results=100&nat=fr";
+var teacherUrl = "https://randomuser.me/api/?results=7&nat=fr";
 
 //Connect the back to the database
 connection.connect(async function (err) {
@@ -45,7 +45,42 @@ connection.connect(async function (err) {
 
       connection.query(sql, function (err, result) {
         if (err) throw err;
-        console.log("1 user inserted");
+        console.log("1 student inserted");
+      });
+    });
+  });
+
+  //do the same for teachers without the age filter
+  await axios.get(teacherUrl).then(function (response) {
+    var result = response.data.results;
+    var dataFiltered = result.filter((user) => user.dob.age >= 30);
+
+    //for each user in the api render,
+    dataFiltered.forEach((result) => {
+      var user = {};
+      if (result.gender == "male") {
+        user.sexe = "m";
+      } else {
+        user.sexe = "f";
+      }
+      user.f_name = result.name.first;
+      user.l_name = result.name.last;
+      user.age = result.dob.age;
+
+      var sql =
+        "INSERT INTO teacher (f_name, l_name, age, sexe) VALUES ('" +
+        user.f_name +
+        "','" +
+        user.l_name +
+        "','" +
+        user.age +
+        "','" +
+        user.sexe +
+        "')";
+
+      connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 teacher inserted");
       });
     });
   });
